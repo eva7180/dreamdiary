cae6309bfa1404b8d0d00fabc9dd2c5a97fbf61b
Eva Stolz <evasmail@gmail.com>
2019-01-05 18:16:32 +0100

    Add more static pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,10 @@
 class StaticPagesController < ApplicationController
 	def impressum
 	end
+
+	def blog
+	end
+
+	def contact
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   root 'articles#landing_page'
 
   get '/impressum', to: 'static_pages#impressum'
+  get '/blog', to: 'static_pages#blog'
+  get '/contact', to: 'static_pages#contact'
 
 end
